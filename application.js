2504d83ea7e17322b72be23b870bf770e63f45d9
amohbata <amohbata@gmail.com>
2014-04-20 01:49:51 +0000

    added user input for locations

--- application.js
+++ application.js
@@ -1,24 +1,50 @@
+// $(function(){
+//   $("#address").keyup(function(event){
+//       if(event.keyCode == 13){
+//           $("#address").click();
+//       }
+//   });
+// });
 
-var vancouver = new google.maps.LatLng(49.2818872,-123.1081877);
-var marker; 
+
+var geocoder;
 var map;
 
 
 function initialize() {
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(49.2818872,-123.1081877);
   var mapOptions = {
-    zoom:13,
-    center:vancouver
+    zoom: 13,
+    center: latlng
   }
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-    marker = new google.maps.Marker({
-  	position:vancouver,
-  	map:map,
-    draggable:true,
-    animation:google.maps.Animation.DROP
+  var marker = new google.maps.Marker({
+	position: latlng,
+	map: map,
+  draggable: true,
+  animation: google.maps.Animation.DROP
   });
   google.maps.event.addListener(marker, 'click', toggleBounce);
 }
+
+function codeAddress() {
+  var address = document.getElementById('address').value;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+
 
 function toggleBounce() {
   if (marker.getAnimation() != null) {
@@ -27,5 +53,12 @@ function toggleBounce() {
     marker.setAnimation(google.maps.Animation.BOUNCE);
   }
 }
+
+function getUserInput() {
+  var userinput = document.getElementById('address').value;
+  alert(userinput);
+}
+
+
 
 google.maps.event.addDomListener(window, 'load', initialize);
